ad401771466308da42a90a3c238d1da43e198544
Agustin Pontura <pontura@gmail.com>
2021-06-29 11:02:38 -0300

    material

---
+++ Assets/scripts/Events.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Events
+{
+    public static System.Action ResetApp = delegate { };
+    public static System.Action HideOldScreens = delegate { };
+    public static System.Action<string> GotoTo = delegate { };
+    public static System.Action<string> GotoBackTo = delegate { };
+    public static System.Action Back = delegate { };
+
+    public static System.Action<string, string, System.Action> PlaySoundTillReady = delegate { };
+    public static System.Action<string, string, bool> PlaySound = delegate { };
+    public static System.Action<string, float> ChangeVolume = delegate { };
+
+    public static System.Action<string> Log = delegate { };
+
+}
+   
